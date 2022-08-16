b0698c81ea76b5d8d68682495d41e6df27cb2377
Nayoung Lee <101040952+na9474@users.noreply.github.com>
2022-08-16 20:22:05 +0900

    [Bronze IV] Title: 주사위 세개, Time: 124 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    
+    public static void main(String[] args)throws IOException{
+        
+        		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+ 
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+    
+        if(a == b && a == c){//a = b = c
+            System.out.println(10000+a*1000);
+        }
+        else if(a != b && a != c && b != c){ //전부 다른경우
+            int max = Math.max(a,Math.max(b,c));
+            System.out.println(max*100);
+        }
+        else if(a == b || a == c) {
+            System.out.println(1000 + a*100);
+        }
+        else{
+            System.out.println(1000 + b*100);
+        }
+    }
+}
