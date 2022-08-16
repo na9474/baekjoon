222615a4e98386cd835d0341e154afe9ad9e7220
Nayoung Lee <101040952+na9474@users.noreply.github.com>
2022-08-16 20:33:03 +0900

    Update 주사위 세개.java

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -5,14 +5,14 @@ public class Main{
     
     public static void main(String[] args)throws IOException{
         
-        		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
- 
-		int a = Integer.parseInt(st.nextToken());
-		int b = Integer.parseInt(st.nextToken());
-		int c = Integer.parseInt(st.nextToken());
-    
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+	int a = Integer.parseInt(st.nextToken());
+	int b = Integer.parseInt(st.nextToken());
+	int c = Integer.parseInt(st.nextToken());
+
         if(a == b && a == c){//a = b = c
             System.out.println(10000+a*1000);
         }
